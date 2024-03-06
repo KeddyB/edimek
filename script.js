3087af6aba4ac61f572e74c5840a203367654a2e
Keddy <bokelusi@gmail.com>
2024-03-06 21:36:45 +0100

    this is the edimeks work first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// document.getElementById('generateButton').addEventListener('click', function() {
+//     var inputText = document.getElementById('wishInput').value;
+//     var outputDiv = document.getElementById('output');
+//     outputDiv.innerHTML = "<p>" + inputText + "</p>";
+//     document.getElementById('wishInput').value = "";
+// });
