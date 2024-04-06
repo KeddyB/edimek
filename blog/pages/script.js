f3e576ded16bbdb91d642b1e6dac5adfe1beb1a4
Keddy <bokelusi@gmail.com>
2024-04-06 22:12:41 +0100

    progress commit

---
+++ blog/pages/script.js
@@ -0,0 +1,41 @@
+const urlParams = new URLSearchParams(window.location.search);
+const slug = urlParams.get('slug');
+
+let PROJECT_ID = "tpxc7pf3"
+let DATASET_NAME = "production"
+let QUERY = encodeURIComponent(`*[_type == "post" && slug.current ="${slug}"]{
+  title,
+  author->{
+    name
+  },
+  _updatedAt,
+  mainImage{
+    asset->{
+      url
+    }
+  },
+}`)
+let URL = `https://${PROJECT_ID}.api.sanity.io/v2022-03-07/data/query/${DATASET_NAME}?query=${QUERY}`
+      
+
+console.log(slug)
+fetch(URL)
+            .then(response => response.json())
+            .then(data => {
+                // Extract the blog post from the data
+                const blogPost = data.result[0];
+
+                // Populate the template with the fetched content
+                const blogPostContent = document.querySelector('.mainBlog');
+                blogPostContent.innerHTML = `
+                    <h1>${blogPost.title}</h1>
+                    <p>Author: ${blogPost.author.name}</p>
+                    <p>Last Updated: ${blogPost._updatedAt}</p>
+                    <img src="${blogPost.mainImage.asset.url}" alt="Main Image">
+                    <p>${blogPost.content}</p>
+                `;
+            })
+            .catch(error => {
+                console.error('Error fetching blog post:', error);
+                // Optionally display an error message or handle the error in another way
+            });
